9d6482478430c60a50e4b42c60cb49c930a27fb4
khairudin07 <khairudinscreamo07@gmail.com>
2020-04-09 10:33:59 +0700

    seed data karyawan

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,11 +12,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
-<<<<<<< HEAD
         $this->call(KaryawanTableSeeder::class);
-=======
         $this->call(GajiTableSeeder::class);
 
->>>>>>> 4aed65f45a9b40eeaff4d81e5efef1918ba8e614
     }
 }
